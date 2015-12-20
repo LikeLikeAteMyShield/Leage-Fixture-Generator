f0afc9a1e303b4ad29d9ecdd24fad1b76df248a3
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2015-12-20 16:45:20 +0000

    adding image assets

---
+++ League Fixture Generator/Club.swift
@@ -0,0 +1,23 @@
+//
+//  Club.swift
+//  League Fixture Generator
+//
+//  Created by Jonny Cameron on 16/12/2015.
+//  Copyright © 2015 Jonny Cameron. All rights reserved.
+//
+
+import Foundation
+
+public class Club {
+
+    let name : String
+    let hasJuniorTeam : Bool
+    let hasSeniorTeam : Bool
+    
+    init(name:String, hasJuniorTeam:Bool, hasSeniorTeam:Bool) {
+    
+        self.name = name
+        self.hasJuniorTeam = hasJuniorTeam
+        self.hasSeniorTeam = hasSeniorTeam
+    }
+}
